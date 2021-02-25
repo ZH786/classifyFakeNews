e80774a0089f44d401c3109d324c665e2f31bf9e
Zubair Hussain <zubairhussain@zubairs-mini.home>
2021-02-25 19:34:50 +0000

    initial completed script

--- classifyFake.py
+++ classifyFake.py
@@ -3,6 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from collections import Counter
+from nltk.tokenize import RegexpTokenizer, word_tokenize, sent_tokenize, TreebankWordTokenizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import train_test_split
+import string
+from nltk.corpus import stopwords
+import nltk
+import spacy
+import en_core_web_sm
 
 rawdf_fke = pd.read_csv("/Users/zubairhussain/Desktop/Python/Datasets/Fake.csv")
 rawdf_fke["Target"] = "FAKE"
@@ -12,7 +23,7 @@ rawdf_tre = pd.read_csv("/Users/zubairhussain/Desktop/Python/Datasets/True.csv")
 rawdf_tre['Target'] = "TRUE"
 
 
-rawdf= pd.concat([rawdf_fke,rawdf_tre])
+rawdf= pd.concat([rawdf_fke,rawdf_tre]) #equivalent to rbind() 
 
 #shuffle dataframe
 rawdf=rawdf.sample(frac=1)
@@ -45,3 +56,55 @@ plt.show()
 rawdf=rawdf[rawdf.textLength!=0][['text','Target','textLength']]
 
 
+def depunctuate(text):
+
+    punc=[]
+
+    for i in text:
+        if i not in string.punctuation:
+            punc.append(i)
+    clean_punc = ''.join(punc)
+
+    return(clean_punc)
+    
+rawdf['text']=rawdf.text.apply(depunctuate)
+
+#nltk.download('averaged_perceptron_tagger')
+#nltk.download('stopwords')
+#nltk.download('punkt')
+
+stop_words = stopwords.words('english')
+
+newstopwords = [depunctuate(word) for word in stop_words]
+
+tknzr = TreebankWordTokenizer()
+rawdf['text']=rawdf['text'].apply(tknzr.tokenize)
+
+def rmStopWords(text):
+    clean = []
+    for i in text:
+        if i not in stop_words:
+            clean.append(i)
+    cleaned = ' '.join(clean)        
+    return(cleaned)        
+
+rawdf['text']=rawdf['text'].apply(rmStopWords) # stop words removed from text field
+
+#Modelling
+X = rawdf.text
+y = rawdf.Target
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3)
+
+#create documentTerm Matrix and TFIDF
+#vect = TfidfVectorizer()
+#X_train_tfidf = vect.fit_transform(X_train)
+
+#SKLEARN pipeline
+txt_clf = Pipeline([('tfidf',TfidfVectorizer()),('clf',LinearSVC())])
+
+txt_clf.fit(X_train, y_train)
+clf_pred = txt_clf.predict(X_test)
+
+print(confusion_matrix(y_test, clf_pred))
+print(classification_report(y_test,clf_pred))
